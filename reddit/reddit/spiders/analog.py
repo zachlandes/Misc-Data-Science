7b985614a8d83fb7652d578c56789e04c5c393e6
zachlandes <zlandes@Zacharys-MBP.gracenote.gracenote.com>
2016-05-27 14:32:46 -0700

    latest
    gets only 39

--- reddit/reddit/spiders/analog.py
+++ reddit/reddit/spiders/analog.py
@@ -8,27 +8,28 @@ class AnalogSpider(CrawlSpider):
     name = "analog"
     allowed_domains = ["www.reddit.com"]
     start_urls = [
-        'http://www.reddit.com/r/analog/new/',
+        'https://www.reddit.com/r/analog/search?sort=new&q=timestamp%3A1400568646..1463727046&restrict_sr=on&syntax=cloudsearch',
     ]
-
     rules = [
     	Rule(LinkExtractor(
-    		allow=['/r/analog/new/\?count=\d*&after=\w*']),
+    		allow=['/r/analog/search\?sort=new&q=timestamp%3A1400568646..1463727046&restrict_sr=on&syntax=cloudsearch&count=\d*&after=\w*',
+            '/r/analog/search\?q=timestamp%3A1400568646..1463727046&restrict_sr=on&sort=new&syntax=cloudsearch&count=\d*&after=\w*',
+            '/r/analog/search\?after=\w*&count=\d*&q=timestamp%3A1400568646..1463727046&restrict_sr=on&sort=new&syntax=cloudsearch']),
     		callback='parse_item', 
     		follow=True)
-    ]
+    ] 
 
     def parse_item(self, response): 
 	    
-        selector_list = response.css('div.thing')
+        #Debugger:
+        #from scrapy.shell import inspect_response
+        #inspect_response(response, self)
         
-        for selector in selector_list:
 
-            item = RedditItem()
+        item = RedditItem()
 
-            item['dates'] = response.xpath('//p[@class="tagline"]/time[@class="live-timestamp"]/@title').extract()
-            item['authors'] = response.xpath('//p[@class="tagline"]/a[@class="author may-blank "]/text()').extract()
-            item['votes'] = response.xpath('//div[@class="midcol unvoted"]/div[@class="score unvoted"]/text()').extract()
-            
-            yield item
+        item['dates'] = response.xpath('//div[@class="search-result-meta"]/span[@class="search-time"]/time/@title').extract()
+        #item['authors'] = response.xpath('//p[@class="tagline"]/a[@class="author may-blank "]/text()').extract()
+        item['votes'] = response.xpath('//div[@class="search-result-meta"]/span[@class="search-score"]/text()').extract()
+        yield item
 		
